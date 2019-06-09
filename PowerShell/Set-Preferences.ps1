f01555d4446904016df38bfc594cef524ce59087
Jean-Claude Parent <noursicain@gmail.com>
2019-06-09 14:38:13 -0400

    Initial commit

---
+++ PowerShell/Set-Preferences.ps1
@@ -0,0 +1,26 @@
+"Application des couleurs personnalisées"
+$CouleurTexte = "Black"
+$CouleurFond ="White"
+
+$Host.PrivateData.ErrorForegroundColor =$CouleurTexte
+$Host.PrivateData.DebugBackgroundColor = $CouleurFond$Host.PrivateData.DebugForegroundColor = $CouleurTexte
+$Host.PrivateData.ErrorBackgroundColor = $CouleurFond
+$Host.PrivateData.ErrorForegroundColor = $CouleurTexte
+$Host.PrivateData.ProgressBackgroundColor = $CouleurFond
+$Host.PrivateData.ProgressForegroundColor = $CouleurTexte
+$Host.PrivateData.VerboseBackgroundColor = $CouleurFond
+$Host.PrivateData.VerboseForegroundColor = $CouleurTexte
+$Host.PrivateData.WarningBackgroundColor = $CouleurFond
+$Host.PrivateData.WarningForegroundColor = $CouleurTexte
+
+
+$Host.PrivateData.ErrorForegroundColor 
+$Host.PrivateData.DebugBackgroundColor = $CouleurFond$Host.PrivateData.DebugForegroundColor = $CouleurTexte
+$Host.PrivateData.ErrorBackgroundColor = $CouleurFond
+$Host.PrivateData.ErrorForegroundColor = $CouleurTexte
+$Host.PrivateData.ProgressBackgroundColor = $CouleurFond
+$Host.PrivateData.ProgressForegroundColor = $CouleurTexte
+$Host.PrivateData.VerboseBackgroundColor = $CouleurFond
+$Host.PrivateData.VerboseForegroundColor = $CouleurTexte
+$Host.PrivateData.WarningBackgroundColor = $CouleurFond
+$Host.PrivateData.WarningForegroundColor = $CouleurTexte
