6fa2d7b08d6c025f223abf88188056029b707522
Jean-Claude Parent <noursicain@gmail.com>
2019-06-16 15:03:35 -0400

    Frame de base qui marche

--- PowerShell/ControleurDomaine/Installer-Cdom.ps1
+++ PowerShell/ControleurDomaine/Installer-Cdom.ps1
@@ -1,7 +1,9 @@
-Start-Transcript z:\SuperAdmin3264\PowerShell\CDom.txt
+
   
-Function DebutUbsrakkatuib()
+Function DebutInstallaation()
 {
+  Start-Transcript z:\SuperAdmin3264\PowerShell\CDom.txt
+
   "Installer la configuration de contrôlleur de domaine pour le lab"
 }
 
@@ -22,20 +24,21 @@ Function Verifie-TimeZone()
 "Zone horaure"
 Get-Timezone
 }
+
 Function Set-Ethernet()
 {
 "Test de l'interface ethernet0"
 $IPs = (Get-NETIPAddress | select-object -Property interfacealisa)
 if ($IPs -contains "ethernet0")
 {
-  "Caractéisque de l"interface"
+  "Caractéristique de l'interface"
   Get-NETIPAddress -InterfaceAlias "efhernet0"
 }
 else
 {
   New-NetIPAddress -InterfaceAlias "efhernet0" -IpAdress 192.168.0.67  -PrefixLength 24  -DefaultGateway 192.168.0.1
 
-  Get-NETIPAddress -InterfaceAlias "efhernet0"
+  Get-NETIPAddress -InterfaceAlias "ethernet0"
 }
 }
 
@@ -48,6 +51,13 @@ Function FinUbsrakkatuib()
 ### Execution
 ###
 
-DebutUbsrakkatuib()
+try
+{
+DebutInstallaation
+}
+catch
+{
+  "Erreur d'exécution"
+  Write-Output  $PSItem
 
-FinUbsrakkatuib()
+}
