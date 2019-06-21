26986b1b511c4c91ba36a15f9cda31e7a6a50429
Jean-Claude Parent <noursicain@gmail.com>
2019-06-21 18:50:08 -0400

    Ébauche des tests

---
+++ PowerShell/Modules/LabSA3264.psm1
@@ -0,0 +1,6 @@
+# cette fonctione ne marche que si l'adresse ip v4 est 2
+Get-IndexIPV4
+{
+
+    2
+}
--- PowerShell/test/Test-IndexIPV4.ps1
+++ PowerShell/test/Test-IndexIPV4.ps1
@@ -1,1 +1,21 @@
-$Resultat = ..\Communs\Get-IndexIPV4.ps1
+### Tests des modules
+try
+{
+  cls
+  $Journal = $PSScriptRoot + '\Log.txt'
+  Start-Transcript $Journal 
+
+  $ModuleAtester = $PSScriptRoot + '\..\Modules\'
+  Write-Output ("Module à tester : " + $ModuleAtester)
+  
+  Import-Module -name  $ModuleAtester -Force -Verbose  
+}
+catch
+{
+  Write-Output $PSItem
+
+}
+
+
+
+#$Resultat = $PSScriptRoot..\Communs\Get-IndexIPV4.ps1
