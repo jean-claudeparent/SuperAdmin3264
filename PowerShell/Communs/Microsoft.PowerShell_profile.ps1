5457442f0ee5ad83006cf47fec3429d85387f02a
Jean-Claude Parent <noursicain@gmail.com>
2019-06-30 10:31:30 -0400

    Ébauche de ma configuration personnalisée

---
+++ PowerShell/Communs/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,44 @@
+Write-Output "Application du profuke"
+"Application des couleurs personnalisées"
+
+$CouleurTexte = "Black"
+
+$CouleurFond ="White"
+
+
+$Host.PrivateData.DebugBackgroundColor = $CouleurFond
+$Host.PrivateData.DebugForegroundColor = $CouleurTexte
+
+$Host.PrivateData.ErrorBackgroundColor = $CouleurFond
+
+$Host.PrivateData.ErrorForegroundColor = $CouleurTexte
+$Host.PrivateData.ProgressBackgroundColor = $CouleurFond
+$Host.PrivateData.ProgressForegroundColor = $CouleurTexte
+
+$Host.PrivateData.VerboseBackgroundColor = $CouleurFond
+
+$Host.PrivateData.VerboseForegroundColor = $CouleurTexte
+
+$Host.PrivateData.WarningBackgroundColor = $CouleurFond
+
+$Host.PrivateData.WarningForegroundColor = $CouleurTexte
+$Host.PrivateData.ErrorForegroundColor =$CouleurTexte
+
+
+$Host.PrivateData.DebugBackgroundColor = $CouleurFond
+$Host.PrivateData.DebugForegroundColor = $CouleurTexte
+
+$Host.PrivateData.ErrorBackgroundColor = $CouleurFond
+
+$Host.PrivateData.ErrorForegroundColor = $CouleurTexte
+$Host.PrivateData.ProgressBackgroundColor = $CouleurFond
+$Host.PrivateData.ProgressForegroundColor = $CouleurTexte
+$Host.PrivateData.VerboseBackgroundColor = $CouleurFond
+
+$Host.PrivateData.VerboseForegroundColor = $CouleurTexte
+
+$Host.PrivateData.WarningBackgroundColor =  $CouleurTexte
+$Host.PrivateData.WarningForegroundColor = $CouleurTexte
+
+cd $PSScriptRoot
+
