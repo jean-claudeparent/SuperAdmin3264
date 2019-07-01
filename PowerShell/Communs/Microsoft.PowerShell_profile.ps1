f1a2b05e274465f699936bfd8b0ecdf10a95ebfd
Jean-Claude Parent <noursicain@gmail.com>
2019-07-01 14:56:56 -0400

    Il reste un bug de couleur $env.quelquechose

--- PowerShell/Communs/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Communs/Microsoft.PowerShell_profile.ps1
@@ -1,44 +1,51 @@
-Write-Output "Application du profuke"
+Write-Output "Application du profile : Profile v 1.0.0"
 "Application des couleurs personnalisées"
 
 $CouleurTexte = "Black"
-
 $CouleurFond ="White"
 
-
+# Debug
 $Host.PrivateData.DebugBackgroundColor = $CouleurFond
 $Host.PrivateData.DebugForegroundColor = $CouleurTexte
 
+# Error
 $Host.PrivateData.ErrorBackgroundColor = $CouleurFond
-
 $Host.PrivateData.ErrorForegroundColor = $CouleurTexte
-$Host.PrivateData.ProgressBackgroundColor = $CouleurFond
-$Host.PrivateData.ProgressForegroundColor = $CouleurTexte
 
+
+# Verbose
 $Host.PrivateData.VerboseBackgroundColor = $CouleurFond
-
 $Host.PrivateData.VerboseForegroundColor = $CouleurTexte
 
+# Warning
 $Host.PrivateData.WarningBackgroundColor = $CouleurFond
-
-$Host.PrivateData.WarningForegroundColor = $CouleurTexte
-$Host.PrivateData.ErrorForegroundColor =$CouleurTexte
-
-
-$Host.PrivateData.DebugBackgroundColor = $CouleurFond
-$Host.PrivateData.DebugForegroundColor = $CouleurTexte
-
-$Host.PrivateData.ErrorBackgroundColor = $CouleurFond
-
-$Host.PrivateData.ErrorForegroundColor = $CouleurTexte
-$Host.PrivateData.ProgressBackgroundColor = $CouleurFond
-$Host.PrivateData.ProgressForegroundColor = $CouleurTexte
-$Host.PrivateData.VerboseBackgroundColor = $CouleurFond
-
-$Host.PrivateData.VerboseForegroundColor = $CouleurTexte
-
-$Host.PrivateData.WarningBackgroundColor =  $CouleurTexte
 $Host.PrivateData.WarningForegroundColor = $CouleurTexte
 
+
+# Consolepane
+# les propriétés existent juste dans ise
+
+#$Host.PrivateData.ConsolePaneBackgroundColor = $CouleurFond
+#$Host.PrivateData.ConsolePaneForegroundColor = $CouleurTexte
+#$Host.PrivateData.ConsolePaneTextBackgroundColor = $CouleurTexte
+
+# Progress
+$Host.PrivateData.ProgressForegroundColor  = $CouleurTexte
+$Host.PrivateData.ProgressBackgroundColor  = $CouleurFond
+
+
+# script
+# Ces propriétés sont définies uniquement dans ise
+try
+{
+  $Host.PrivateData.ScriptPaneBackgroundColor = $CouleurFond
+  hyn$Host.PrivateData.ScriptPaneForegroundColor = $CouleurFond
+}
+catch
+{
+# c'est normal de ne rien faire dans le catch
+
+}
+# $Host.PrivateData.TokenColors = $CouleurTex
 cd $PSScriptRoot
-
+# $Host.PrivateData
